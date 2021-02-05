a5cb5742ca76509033c9bd08e7187d88d245ba79
emarchite <78593860+emarchite@users.noreply.github.com>
2021-02-05 18:29:10 +0100

    Añadimos enrutamiento logout y añadimos un elemento al menu principal -> logout

--- src/app/components/principal/principal-routing.module.ts
+++ src/app/components/principal/principal-routing.module.ts
@@ -4,13 +4,16 @@ import { ShipsComponent } from '../ships/ships.component';
 import { PageOneComponent } from '../page-one/page-one.component';
 import { PageTwoComponent } from '../page-two/page-two.component';
 import { PrincipalComponent } from './principal.component';
+import { LogoutComponent } from '../logout/logout.component';
 
 const routes: Routes = [
-  { path: '', component: PrincipalComponent,
+  { 
+    path: '', component: PrincipalComponent,
   children: [
     { path: 'ships', component: ShipsComponent },
     { path: 'pageOne', component: PageOneComponent },
     { path: 'pageTwo', component: PageTwoComponent },
+    { path: 'logout', component: LogoutComponent },
   ] }
 ];
 
