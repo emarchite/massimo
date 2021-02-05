8de6ae29028ac8a9d747f6fa0f5f33c3a02ef775
emarchite <78593860+emarchite@users.noreply.github.com>
2021-02-05 23:46:36 +0100

    Servicio, Componente, Vista -> Pages
    Muestra un listado de las categorias pagina

---
+++ src/app/components/pages/pages.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PagesService } from '../../services/pages/pages.service';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.scss']
+})
+export class PagesComponent implements OnInit {
+
+  public dataPages: any = [];
+
+  constructor(private router: Router, private pagesService: PagesService) { }
+
+  ngOnInit(): void {
+
+
+    this.pagesService.getPages().subscribe((data) => {
+
+      // normalizamos a un array de objetos con asociacion
+      this.dataPages = Object.entries(data);  
+      let aux = [];
+      this.dataPages.forEach((item, index) => {
+        aux[index] = { "title": item[0], "url": item[1]}
+      });
+
+
+      this.dataPages = aux;
+      console.log(this.dataPages);
+    })
+
+  }
+
+}
